d9afb6845c38e905ef36ecaa1c54b899659c61ee
Woosang <yws1502@naver.com>
2021-12-21 00:32:16 +0900

    Setting media query in login, join, signup page

--- js/main/main.js
+++ js/main/main.js
@@ -1,3 +1,11 @@
+// 최상단 광고 배너 삭제 버튼
+const topAdBanner = document.querySelector(".advertisement");
+const topAdBtn = topAdBanner.querySelector(".btn-closeAd");
+topAdBtn.addEventListener("click", () => {
+  topAdBanner.style.display = "none";
+})
+
+// 전역 네비게이션 바 상단 fixed
 const contentInfo = document.querySelector('.gnb');
 const contentInfoHeight = contentInfo.getBoundingClientRect().bottom;
 document.addEventListener('scroll', () => {
